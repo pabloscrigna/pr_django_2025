8b054e187266605aa3b5e00f33c68d4b5625790e
Pablo Scrigna <pscrigna@gruposimpli.com>
2025-11-06 19:29:22 -0300

    archivos del 5/11

--- iades/alumnos/urls.py
+++ iades/alumnos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import alumnos_vista, alumnos_agregar
+from .views import alumnos_vista, alumnos_agregar, alumnos_listar
 
 urlpatterns = [
-    path("listar/", alumnos_vista),
+    path("listar/", alumnos_listar),
     path("agregar/", alumnos_agregar),
     path("eliminar/", alumnos_vista),
     path("", alumnos_vista)
--- iades/alumnos/views.py
+++ iades/alumnos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import HttpResponse
 
 # Create your views here.
+from .models import Alumno
 
 
 def alumnos_vista(request):
@@ -8,4 +9,24 @@ def alumnos_vista(request):
 
 
 def alumnos_agregar(request):
-    return HttpResponse("En construccion agregar un alumno")
+
+    nombre = "Micaela"
+    edad = 20
+
+    alumno = Alumno()
+    alumno.nombre = nombre
+    alumno.edad = edad
+
+    alumno.save()
+
+    return HttpResponse(f"Se creo el usuario {alumno.nombre} exitosamente")
+
+
+def alumnos_listar(request):
+
+    alumnos = Alumno.objects.all()
+
+    for alumno in alumnos:
+        print(alumno.nombre)
+
+    return HttpResponse(f"Se listaron los alumnos exitosamente")
